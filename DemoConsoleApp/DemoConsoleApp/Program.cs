a19cac6ebe6892bc88c0244a031dabc12ee235ec
ddecours <doug_decoursin@hotmail.com>
2016-06-06 10:12:34 -0500

    Update Program.cs

--- DemoConsoleApp/DemoConsoleApp/Program.cs
+++ DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace DemoConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello Doug!");
             /// More
 
         }
